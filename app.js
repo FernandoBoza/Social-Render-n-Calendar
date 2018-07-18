f20455cd01a484310b9a8ceec7ef392cd4683202
Fernando <hi@fernandoboza.com>
2018-07-18 15:10:26 -0400

    prepare for deploy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/api/userRoutes');
 const clientRoutes = require('./routes/api/clientsRoute');
 const passport = require('passport');
+const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -32,4 +33,14 @@ require('./config/passport')(passport);
 // Use Routes
 app.use('/api/users', userRoutes);
 app.use('/api/clients', clientRoutes);
+
+// Server Static Assets If In Production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
