55353b0578b25034e8c8cb19168cfa3d4c959ee7
Fernando <hi@fernandoboza.com>
2018-07-18 16:15:43 -0400

    fixing the REDUX dev tools issue

--- client/src/store.js
+++ client/src/store.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index.js';
 
-const middleware = [thunk];
 const intitialState = {};
+const middleware = [thunk];
 const store = createStore(
   rootReducer,
   intitialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   )
 );
 
