b05bc5ad3a3d043f1717b88dd08902c16328179d
Fernando <hi@fernandoboza.com>
2018-07-20 16:41:53 -0400

    added message proptype to Spinner Componenet

--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default () => {
-  // return <img className="spinner" src="https://gifimage.net/wp-content/uploads/2018/05/spinner-gif-transparent-background-13.gif" alt="loading..." />;
+const Spinner = ({ message }) => {
   return (
-    <h4 className="display-4">
-      If loading doesn't go away, hit refresh <i className="text-info fa fa-spinner fa-pulse fa-3x fa-fw" />
+    <h4 className="display-4 text-center">
+      {message}
+      <br />
+      <i className=" mt-5 text-info fa fa-spinner fa-pulse fa-3x fa-fw" />
     </h4>
   );
 };
+
+Spinner.propTypes = {
+  message: PropTypes.string
+};
+
+Spinner.defaultProps = {
+  message: "If loading doesn't go away, hit refresh"
+};
+
+export default Spinner;
+// return <img className="spinner" src="https://gifimage.net/wp-content/uploads/2018/05/spinner-gif-transparent-background-13.gif" alt="loading..." />;
