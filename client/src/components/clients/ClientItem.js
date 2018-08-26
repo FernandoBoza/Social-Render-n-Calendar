0108873ef148e4c7970b4f3974b1175df9dd3a1d
Fernando <hi@fernandoboza.com>
2018-08-26 18:24:10 -0400

    implemented WOW and Animate CSS to dev branch

--- client/src/components/clients/ClientItem.js
+++ client/src/components/clients/ClientItem.js
@@ -6,17 +6,17 @@ export default class ClientItem extends Component {
   render() {
     const { clients } = this.props;
     return (
-      <div className="card card-body bg-light mb-3">
+      <div className="card card-body bg-light mb-3 wow animated fadeInRight" data-wow-duration="2s" data-wow-delay="1s">
         <div className="row">
           <div className="col-md-12">
             <p>
               <b>{clients.name}: </b> Updated by {clients.lastUpdatedBy}
             </p>
-            <Link to={`/clients/${clients.handle}`} className="btn btn-primary">
+            <Link to={`/clients/${clients.handle}`} className="btn btn-primary wow animated fadeInRight" data-wow-duration="2s" data-wow-delay="1.5s">
               <b>View Client</b> <i className="fa fa-user" />
             </Link>
 
-            <Link to={`/content-calendar/${clients.handle}`} className="btn btn-success mx-3">
+            <Link to={`/content-calendar/${clients.handle}`} className="btn btn-success mx-3 wow animated fadeInRight" data-wow-duration="2s" data-wow-delay="1.5s">
               <b>View Client Calendar</b> <i className="fa fa-calendar-plus-o" />
             </Link>
           </div>
