d0eb7bb860e2aacd0c61beee022233cf6e8ec31e
Fernando <hi@fernandoboza.com>
2018-09-13 23:37:32 -0400

    fixed the update post

--- routes/api/socialRenderRoute.js
+++ routes/api/socialRenderRoute.js
@@ -64,29 +64,33 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     .catch(err => console.log(err));
 });
 
-// @POST api/social-render/id/_id
+// @PUT api/social-render/id/_id
 // @desc UPDATE A SOCIAL CONTENT BY ID
 router.put('/id/:_id/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { errorsObj, isValid } = validateSocialRender(req.body);
   const trimHandle = n => n.toLowerCase().replace(/\s/g, '_');
   const contentUpdates = {};
-  if (!isValid) {
-    return res.status(404).json(errorsObj);
-  }
+  // if (!isValid) {
+  //   return res.status(404).json(errorsObj);
+  // }
 
   contentUpdates._id = req.params._id;
   if (req.body.clientName) contentUpdates.clientName = req.body.clientName;
   if (req.body.clientInitials) contentUpdates.clientInitials = req.body.clientInitials;
   if (req.body.clientHandle) contentUpdates.clientHandle = trimHandle(req.body.clientName);
-  if (req.body.contentCopy) contentUpdates.contentCopy = req.body.contentCopy;
-  if (req.body.contentTwitterCopy) contentUpdates.contentTwitterCopy = req.body.contentTwitterCopy;
-  if (req.body.contentLinkedInCopy) contentUpdates.contentLinkedInCopy = req.body.contentLinkedInCopy;
-  if (req.body.contentInstagramCopy) contentUpdates.contentInstagramCopy = req.body.contentInstagramCopy;
-  if (req.body.imgLink) contentUpdates.imgLink = req.body.imgLink;
-  if (req.body.imgLinkInstagram) contentUpdates.imgLinkInstagram = req.body.imgLinkInstagram;
+  contentUpdates.contentCopy = req.body.contentCopy;
+  contentUpdates.contentTwitterCopy = req.body.contentTwitterCopy;
+  contentUpdates.contentLinkedInCopy = req.body.contentLinkedInCopy;
+  contentUpdates.contentInstagramCopy = req.body.contentInstagramCopy;
+  contentUpdates.imgLink = req.body.imgLink;
+  contentUpdates.imgLinkInstagram = req.body.imgLinkInstagram;
   if (req.body.dateGoingLive) contentUpdates.dateGoingLive = req.body.dateGoingLive;
 
-  SocialRenderModel.findOneAndUpdate({ _id: contentUpdates._id }, { $set: contentUpdates }, { new: true })
+  SocialRenderModel.findOneAndUpdate(
+    { _id: contentUpdates._id },
+    { $set: contentUpdates },
+    { new: true }
+  )
     .then(client => res.json(client))
     .catch(err => res.status(404).json(err));
 });
